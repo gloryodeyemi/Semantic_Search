c8bab1b93baff7666adf4e5af0124b5399cdcfcd
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-12-06 19:04:05 -0500

    Semantic search

--- FAISS/utils/feature_logic.py
+++ FAISS/utils/feature_logic.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # function to fetch papers starting with a given letter
 def fetch_papers_by_alphabet(letter):
-    dataset = pd.read_csv('../data/df-data.csv', dtype={'id': str})
+    dataset = pd.read_csv('data/df-data.csv', dtype={'id': str})
 
     # filter papers by titles starting with the given letter
     filtered_papers = dataset[dataset['title'].str.startswith(letter, na=False)]
